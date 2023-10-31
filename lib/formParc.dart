86b932d63ab3151a03da58cfc6c329de59a39985
eduardoMeneghetti <132016082+eduardoMeneghetti@users.noreply.github.com>
2023-10-31 17:03:25 -0300

    ultimas mudanças realizadas
    Última versão realizada no projeto, no entanto, este, foi descontinuado.

--- lib/formParc.dart
+++ lib/formParc.dart
@@ -1,7 +1,8 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 class FormParc extends StatefulWidget {
-  const FormParc({super.key});
+  final Function(int) onCadastroPressed; // Callback para o botão de cadastro
+  const FormParc({super.key, required this.onCadastroPressed});
 
   @override
   State<FormParc> createState() => _FormParcState();
@@ -14,35 +15,42 @@ class _FormParcState extends State<FormParc> {
   Widget build(BuildContext context) {
     return Material(
       child: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  controller: _controller,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: 'Código do Parceiro',
-                    border: OutlineInputBorder(),
+        width: double.infinity,
+        height: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Código do Parceiro',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  String dadosUser = _controller.text;
+                  int codParceio = int.parse(dadosUser);
+                  print('O usuário digitou $dadosUser');
+
+                  // Chama o callback passando o código do parceiro para a tela principal (HomeUser)
+                  widget.onCadastroPressed(codParceio);
+                },
+                child: Text('Cadastrar'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Colors.green.shade300,
                   ),
                 ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    String dadosUser = _controller.text;
-                    print('O usuário digitou $dadosUser');
-                  },
-                  child: Text('Pesquisar'),
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Colors.green.shade300)),
-                ),
-              ],
-            ),
-          )),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/homeUser.dart
+++ lib/homeUser.dart
@@ -1,8 +1,13 @@
-import "package:app_localizacao/formParc.dart";
-import "package:app_localizacao/localizacaoUser.dart";
-import "package:flutter/material.dart";
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:app_localizacao/formParc.dart';
+import 'package:app_localizacao/localizacaoUser.dart';
 
 class HomeUser extends StatelessWidget {
+  UserLocalizacao _userLocalizacao = UserLocalizacao();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,15 +15,46 @@ class HomeUser extends StatelessWidget {
         title: Text('Cassul Distribuidora'),
         backgroundColor: Colors.green.shade300,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          UserLocalizacao userLocalizacao = UserLocalizacao();
-          userLocalizacao.getPosicao();
+      floatingActionButton: ElevatedButton(
+        onPressed: () async {
+          await _userLocalizacao
+              .getPosicao(); // Aguarde a obtenção da posição do usuário
+          FormParc formParc = FormParc(onCadastroPressed: (codParceio) {
+            // Callback chamado quando o botão de cadastro no FormParc for pressionado
+            postData(codParceio, _userLocalizacao.lat, _userLocalizacao.long);
+          });
+
+          // Mostrar o formulário como um diálogo
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(child: formParc);
+            },
+          );
         },
-        child: Icon(Icons.add_location),
-        backgroundColor: Colors.green.shade300,
+        child: Text('Obter localicação'),
+        style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.green.shade300)),
       ),
-      body: FormParc(),
     );
+  }
+
+  Future<void> postData(int codParc, double lat, double long) async {
+    var url = 'http://192.168.15.126/crud/';
+    var headers = {'Content-Type': 'application/json'};
+    var body =
+        jsonEncode({'codParc': codParc, 'latitude': lat, 'longitude': long});
+
+    var response =
+        await http.post(Uri.parse(url), headers: headers, body: body);
+
+    if (response.statusCode == 200) {
+      // caso o status seja positivo
+      print('Resposta do servidor: ${response.body}');
+    } else {
+      // caso a resposta seja contraria a positivo
+      print('Falha na solicitação. Código de status: ${response.statusCode}');
+    }
   }
 }
